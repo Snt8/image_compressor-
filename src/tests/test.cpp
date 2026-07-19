d3181ab8ee1581d382948e3d82700e7915076fd8
Snt <sant85082@gmail.com>
2026-07-18 19:44:41 -0500

    feat(test): implement compressing function testing

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,3 +1,17 @@
-//
-// Created by sntav on 18/07/2026.
-//
+#include "../core/compressor.h"
+#include "../processor/image_processor.hpp"
+#include <string>
+#include <iostream>
+
+int main() {
+    std::string image = "../src/tests/images/new_york_test_image.jpg";
+    int quality = 50;
+
+    if (compress_image(image.c_str(), quality)) {
+        std::cout << "The image was compressed successful";
+    }
+
+    else {
+        std::cout << "Error compressing the image";
+    }
+}
